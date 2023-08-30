7a0c9df46d1b785eacaf707b0662e307ba724083
ayushthunder14 <ayushthunder14@gmail.com>
2023-08-30 09:35:43 +0530

    intital commit react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        {"Welcome to Stormwoods"}
+      </h1>
+      <p>{"The great place to get your wood works"}</p>
+    </div>
+  );
+}
+
+export default App;
